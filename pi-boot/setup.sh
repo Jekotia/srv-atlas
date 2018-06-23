0d3cd4149c3dc256a739aa22104d0fcb6d301a2f
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-06-23 19:09:34 +0000

    Add custom config entries to pi's boot.txt and copy my edid.dat to the /boot partition

--- pi-boot/setup.sh
+++ pi-boot/setup.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-_root="/atlas"
 
 ## Pi boot settings
 piConfig="/boot/config.txt"
 
-echo "hdmi_force_hotplug=1" > ${piConfig}
-echo "hdmi_edid_file=1" > ${piConfig}
+echo >> ${piConfig}
+echo "## Begin pi-boot/setup.sh" >> ${piConfig}
+echo "hdmi_force_hotplug=1" >> ${piConfig}
+echo "hdmi_edid_file=1" >> ${piConfig}
+echo "## End pi-boot/setup.sh" >> ${piConfig}
 
-cp -a ${_root}/pi-boot/edid.dat /boot/
+cp -a ${_common}/pi-boot/edid.dat /boot/
